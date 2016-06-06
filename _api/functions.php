ddbc57701ec257f3c243740dadc562438e8f7765
Kay Gonschior <kay.gonschior@googlemail.com>
2016-06-06 15:56:05 +0200

    progress on api #37

--- _api/functions.php
+++ _api/functions.php
@@ -4,9 +4,11 @@ class Constants
     const mySql_host = "localhost:3036";
     const mySql_user = "isitcool";
     const mySql_key = "?4n2+A@-X:PyKp`Q>Gs}nf*6";
+    const mySql_dbname = 'isitcool';
 }
 
 function cleanInput($input/*String*/){
+    $input = strtolower($input);
     return trim($input);
 }
 
@@ -30,6 +32,34 @@ function generateCoolnessForWord($word){
     return intval($retVal);
 }
 
+function tryInsertWordIntoDb_default($word, $coolness){
+    $shouldInsert = false;
+
+    $conn = mysql_connect(Constants::mySql_host, Constants::mySql_user, Constants::mySql_key);
+    if( !$conn ){
+        die('Could not connect: ' . mysql_error());
+    }
+    mysql_select_db(Constants::mySql_dbname, $conn);
+    $sql = "SELECT value FROM words WHERE word = '".$word."'";
+    $sqlReturn = mysql_query($sql, $conn);
+    if( !$sqlReturn ){
+        die('Could not get data: ' . mysql_error());
+    }
+    else if( !($row = mysql_fetch_array($sqlReturn, MYSQL_ASSOC)) ){
+        $shouldInsert = true;
+    }
+
+    if($shouldInsert){
+        $sql = "INSERT INTO words (word, value, value_default, upvotes, downvotes) VALUES ('".$word."', '".$coolness."', '".$coolness."', '0', '0')";
+        $retval = mysql_query( $sql, $conn );
+        if(! $retval ){
+            die('Could not enter data: ' . mysql_error());
+        }
+    }
+
+    mysql_close($conn);
+}
+
 function getCoolnessForWord($word){
     $coolness = -1;
 
@@ -37,21 +67,21 @@ function getCoolnessForWord($word){
     if( !$conn ){
         die('Could not connect: ' . mysql_error());
     }
-    mysql_select_db()
-    $sql = "SELECT value FROM words WHERE word = " + word;
-    print_r($conn);
-    $sqlResult = $conn->query($sql);
-    if ($sqlResult->num_rows > 0){
-        if($sqlResult->num_rows > 1){
-            die("more than one result");
-        }
-        $row = $sqlResult->fetch_assoc();
-        $coolness = $row["value"];
+    mysql_select_db(Constants::mySql_dbname, $conn);
+    $sql = "SELECT value FROM words WHERE word = '".$word."'";
+    $sqlReturn = mysql_query($sql, $conn);
+    if( !$sqlReturn ){
+        die('Could not get data: ' . mysql_error());
     }
+    else if( $row = mysql_fetch_array($sqlReturn, MYSQL_ASSOC) ){
+        $coolness = intval($row['value']);
+    }
+
     mysql_close($conn);
 
     if($coolness==-1){
         $coolness = generateCoolnessForWord($word);
+        tryInsertWordIntoDb_default($word, $coolness);
     }
 
     return $coolness;
