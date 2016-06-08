69c55dd714f04c82a16e3aafe2ae24ddc6e1ff4f
Kay Gonschior <kay.gonschior@googlemail.com>
2016-06-08 18:12:10 +0200

    remove google login code

--- js/pagescript-vote.js
+++ js/pagescript-vote.js
@@ -1,4 +1,3 @@
-function onSignIn(googleUser) { isitcool.vote.onSignInWithGoogle(googleUser); }
 var isitcool = {
     vote: {
         login_state: false, /*false - not logged in, true - logged in*/
@@ -39,29 +38,11 @@ var isitcool = {
             });
         },
 
-        onSignInWithGoogle: function(googleUser) {
-            console.log("onSignInWithGoogle");
-            var id_token = googleUser.getAuthResponse().id_token;
-            /*POST*/
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', 'http://isitcool.bplaced.net/login.php');/*s*/
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.onload = function() {
-                console.log('Signed in as: ' + xhr.responseText);
-                isitcool.vote.updateLoginAndVoteState();
-            };
-            xhr.send('action=login&idtoken=' + id_token);
-        },
-
         vote: function(bool/*false - down, true - up*/) {
             if(bool){
                 console.log("vote: up");
             }else{
                 console.log("vote: down");
-                var auth2 = gapi.auth2.getAuthInstance();
-                auth2.signOut().then(function () {
-                  console.log('User signed out.');
-                });
             }
         },
 
