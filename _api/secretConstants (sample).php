e33820866871a67373d44b9ad4d0812c2a13738a
Kay Gonschior <info@kay-gonschior.de>
2020-01-15 17:42:47 +0100

    make it work again

---
+++ _api/secretConstants (sample).php
@@ -0,0 +1,10 @@
+<?php
+/*http://isitcool.bplaced.net*/
+class Constants
+{
+    const mySql_host = "localhost";
+    const mySql_user = "isitcool";
+    const mySql_key = "*********";
+    const mySql_dbname = 'isitcool';
+}
+?>
